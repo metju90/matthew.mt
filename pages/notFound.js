7920e35c07162caf78b52718d9c0cb36e7fab0b7
Matthew <barbaramatthew90@gmail.com>
2020-05-05 02:44:35 +0200

    Wrapped text in container and added margin for page notFound

--- pages/notFound.js
+++ pages/notFound.js
@@ -1,13 +1,17 @@
 /** @format */
+import "/components/container.js";
 
 const template = document.createElement("template");
 template.innerHTML = `
     <style>
       :host {
         display: block;
+        margin: 50px 0;
       }
     </style>
-    The page you are trying to reach was not found. 
+    <x-container>
+        The page you are trying to reach was not found. 
+    </x-container>
 `;
 
 class NotFound extends HTMLElement {
