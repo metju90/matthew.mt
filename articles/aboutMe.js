0521560b627fc42e38321b1611152de93c498b0e
Matthew <barbaramatthew90@gmail.com>
2021-08-02 07:59:53 +0200

    Reworded the 'about me' blog

--- articles/aboutMe.js
+++ articles/aboutMe.js
@@ -3,7 +3,7 @@ const title = "About me";
 const content = /* html */ `
   <p>My full name is Matthew Barbara. For the last ${workingExperience} i've been working as a fullstack developer. My hobbies are skiing, hiking, gym, playing video games and learning more about software development. I was born and raised in Malta and I've been living in Switzerland since 2018.</p>
   <p>
-      Prior to working as a software engineer I used to work as croupier in two casinos in Malta. Dealing games such as roulette, blackjack and at later stages Texas hold 'em poker too. After 4 years working in casino, at the age of 22, I realized that this isn’t what I want to do for the rest of my life. I found myself asking what new profession I could take on if I had to stop working as a croupier.
+    Prior to working as a software engineer, I used to work as a croupier in two casinos in Malta. Dealing games such as roulette, blackjack and at later stages, Texas hold 'em poker too. After four years, aged 22, I realised that this isn’t what I want to do for the rest of my life. I found myself asking what new profession I could take on if I had to stop working as a croupier.
   </p>
   <figure>
       <img
@@ -15,35 +15,34 @@ const content = /* html */ `
       </figcaption>
   </figure>
   <p>
-      Being my extended family's uncertified but effective PC technician, I did know the basics of computers by around the age of 12. I knew what RAM, CPU and hard disk were and why a computer may be performing slow. I also knew that if an Ethernet cable is unplugged it was very likely to be the reason for having no internet connection. Ohh, and I certainly knew how to format hard disks! That was my solution to PCs with janky performance problems. It worked most of the time. When it didn't, I told my clients to go to a real technician.😁
+    Being my extended family's uncertified but effective PC technician, I did know the basics of computers by around the time I was 12 years old. I knew what RAM, CPU and hard disk were and why a computer may be performing slow. I also knew if your Ethernet cable was unplugged, it might be the reason why you have no internet connection. Ohh, and I certainly knew how to format hard disks! That was my solution to PCs with janky performance problems. It worked most of the time. When it didn't, I told my clients to go to a real technician.😁
   </p>
   <p>
-      Back to me being a croupier and wanting to change my profession - I always loved computers. I had fallen in love with computers when my family bought a home PC in 2002. My love for computers grew even stronger in my late teenage when I was assembling gaming computer hardware for myself and friends. The idea of me working as an IT professional was intriguing. I started to look in which field I could work as an IT professional. Do I become the certified PC technician that I never was? 🤔 Or perhaps should I look at other jobs in IT? What if I become a programmer? But how exactly? These were the questions that I was asking myself and that was the time when I started to become curious about software. 
+    Back to me being a croupier and wanting to change my profession - I always loved computers. I had fallen in love with computers when my family bought a home PC in 2002. My passion for computers grew even more in my late teens when I assembled gaming computer hardware for myself and my friends. The idea of me working as an IT professional was intriguing. I started to look in which field I could work as an IT professional. Do I become the certified PC technician that I never was? 🤔 Or perhaps should I look at other jobs in IT? What if I become a programmer? But how exactly? These were the questions that I was asking myself and that was when I started to become curious about software.
   </p>
   <p>
-      Java was the first programming language I came across with. I remember being very excited after (a long time) I managed successfully execute a hello world app on my machine. Then I took it further. In few weeks I built several micro-applications which runs via the terminal. Then I came across HTML and CSS. I found these two much easier and in a few days I had built an amateur website for a fictitious restaurant and it's menu was full of food which I was cooking at home at that time. I was taking photos of each meal I cooked to use them on my experimental web project.☺️ By this time I was sure that I wanted to be a software engineer. The question was no longer in which field I can work in IT. The question was what programming language I should focus on. At that time I had chosen Java.
+    Java was the first programming language I came across with. I remember being very excited after (a long time) I successfully executed a hello world app on my machine. Then I took it further. In few weeks, I built several micro-applications which runs via the terminal. Then I came across HTML and CSS. I found these two easier to deal wih and in a few days, I had built an amateur website for a fictitious restaurant, and it's menu was full of food that I was cooking at home at that time. I was taking photos of each meal I cooked to use them on my experimental web project.☺️ By this time, I was sure that I wanted to be a software engineer. The question was no longer in which field I can work in IT. The question was what programming language I should focus on. At that time, I had chosen Java.
   </p>
   <figure>
       <img src="/images/first-web-menu.png" />
       <figcaption>
-      Background color, italic fonts and anchor tags which links to internal pages. This is part of what I built days after I heard about "HTML" and "CSS"
+        Background color, italic fonts and anchor tags - first learnings
       </figcaption>
   </figure>
   <figure>
       <img src="/images/first-web-item.png" />
       <figcaption>
-        Using the image tag was another milestone that I reached on the same
-        day. 🎉
+        And of course, I learned how to load images too. 🎉
       </figcaption>
   </figure>
   <p>
-     In 2012 I began studying to get an International diploma in computing. After graduating, in 2013,  I started studying for BSc. Computer Science(System Engineering). The degree focused more on system engineering rather than the fundamentals of computer science. However, at that time I believed it was sufficient to land me my dream job at the time - working as a Java developer.
+    In 2012 I began studying to get an International diploma in computing. After graduating, in 2013,  I started studying for BSc. Computer Science(System Engineering). The degree focused more on system engineering rather than the fundamentals of computer science. However, I believed it was sufficient to land me my dream job at the time - working as a Java developer.
   </p>
   <p>
-     While studying for a bachelor degree, at the end of the year 2014 I was offered a job opportunity as a full stack developer. There I was working with PHP and frontend technologies. It wasn't exactly my ambition to work as a software developer if I wasn't going to use Java as a programming language, but I saw this as a great opportunity to start with and I accepted the offer.
+    While studying for a bachelor degree, at the end of the year 2014, I was offered a job opportunity as a full stack developer. There I was working with PHP and frontend technologies. It wasn't precisely my ambition to work as a software developer if I wasn't going to use Java as a programming language but I have, nevertheless, saw this as a great opportunity to start with and I accepted the offer.
   </p>
   <p>
-      Fast forwarding to today - I have never looked back. I've spent an amazing 6 years working as a professional full stack developer. It has been an incredible journey and the ever-changing technologies related to web applications fascinates me.
+    Fast-forwarding to today - I have never looked back. I've spent an incredible six years working as a professional full stack developer. It has been an amazing journey and the ever-changing technologies related to web applications fascinates me.
   </p>
 `;
 export { title, content };
