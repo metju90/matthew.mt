53813365ec57e86876e675322f55419fb53d9759
Matthew <barbaramatthew90@gmail.com>
2021-08-19 07:07:28 +0200

    Updated chrome devtools article's redirection

--- articles/chromeDevtools.js
+++ articles/chromeDevtools.js
@@ -3,7 +3,7 @@ const summary = "Chrome DevTools feature highlights and shortcuts";
 const date = "August 14th, 2021";
 const lastUpdated = "";
 const link = "/chrome-devtools";
-const redirectUrl =
-  "https://dev.to/metju90/chrome-devtools-highlights-and-shortcuts-5bh7";
+const redirectUrl = "https://matthew.mt/chrome-devtools";
+//   "https://dev.to/metju90/chrome-devtools-highlights-and-shortcuts-5bh7";
 
 export { redirectUrl, title, summary, date, lastUpdated, link };
